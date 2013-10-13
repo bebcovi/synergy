d9c985362780b6930a9790deeb0b88f05ca63562
Janko Marohnić <janko.marohnic@gmail.com>
2013-10-13 16:05:19 +0200

    Validate uniqueness of texts

--- app/models/text.rb
+++ app/models/text.rb
@@ -1,6 +1,8 @@
 class Text < ActiveRecord::Base
   before_destroy { false } # Forbid deletion
 
+  validates_uniqueness_of :action, scope: :controller
+
   def self.find(uid)
     begin
       super(Integer(uid))
